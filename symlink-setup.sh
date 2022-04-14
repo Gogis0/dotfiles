4174e70714f120135b60cacb604dfea423f4bee1
Gogis0 <adriangoga0@gmail.com>
2022-04-14 20:03:45 +0200

    Remove redundant stuff from the install script

--- symlink-setup.sh
+++ symlink-setup.sh
@@ -15,61 +15,15 @@ answer_is_yes() {
         || return 1
 }
 
-ask() {
-    print_question "$1"
-    read
-}
-
 ask_for_confirmation() {
     print_question "$1 (y/n)"
     read -n 1
     printf "\n"
 }
 
-ask_for_sudo() {
-    # Ask for the administrator password upfornt
-
-    # Update existing 'sudo' timestamp until this script has finished
-    while true; do
-        sudo -n true
-        sleep 60
-        kill -0 "$$" - || exit
-    done &> /dev/null &
-}
-
-cmd_exists() {
-    [ -x "$(command -v "$1")" ] \
-        && printf 0 \
-        || printf 1
-}
-
 execute() {
     $1 &> /dev/null
     print_result $? "${2:-$1}"
-}
-
-get_answer() {
-    printf "$REPLY"
-}
-
-is_git_repository() {
-    [ "$(git rev-parse &> /dev/null; printf $?)" -eq 0 ] \
-        && return 0 \
-        || return 1
-}
-
-mkd() {
-    if [ -n "$1" ]; then
-        if [ -e "$1" ]; then
-            if [ ! -d "$1" ]; then
-                print_error "$1 - a file with the same name already exists!"
-            else
-                print_success "$1"
-            fi
-        else
-            execute "mkdir -p $1" "$1"
-        fi
-    fi
 }
 
 print_error() {
@@ -109,19 +63,16 @@ declare -a FILES_TO_SYMLINK=$(find . -type f -name ".*" -not -name .git | sed -e
 FILES_TO_SYMLINK="$FILES_TO_SYMLINK .vim" # add /.vim
 
 main() {
-
     local file=""
     local sourceFile=""
     local targetFile=""
 
     for file in ${FILES_TO_SYMLINK[@]}; do
-
         sourceFile="$(pwd)/$file"
         targetFile="$HOME/$(printf "%s" "$file" | sed "s/.*\/\(.*\)/\1/g")"
 
         if [ -e "$targetFile" ]; then
             if [ "$(readlink "$targetFile")" != "$sourceFile" ]; then
-                
                 ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
                 if answer_is_yes; then
                     rm -rf "$targetFile"
